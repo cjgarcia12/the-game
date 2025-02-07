94bcb49df6baf4833c43e542a05428e2e112b8ca
cjgarcia12 <christian.jgarcia03@gmail.com>
2025-02-07 16:43:40 -0500

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <View className='w-full h-full bg-[#DEE2E6] flex items-center justify-center'>
+            <Text className='text-cyan-300'>Index</Text>
+        </View>
+    )
+}
+export default Index
