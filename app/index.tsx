6dd9dae6d4c1e936740b1c07593fa81e6158e70e
cjgarcia12 <christian.jgarcia03@gmail.com>
2025-02-07 16:57:08 -0500

    Home page in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,21 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const Index = () => {
     return (
-        <View className='w-full h-full bg-[#DEE2E6] flex items-center justify-center'>
-            <Text className='text-cyan-300'>Index</Text>
+        <View className="flex-1 bg-[#495057] items-center justify-center">
+            <View className="px-8 py-4 bg-[#ADB5BD] rounded-2xl shadow-md">
+                <Text className="text-[#212529] text-2xl font-bold text-center mb-4">
+                    Welcome to The Game
+                </Text>
+                <Text className="text-[#495057] text-lg text-center">
+                    Close this app immediately.
+                    <Text className="font-bold"> Don’t open it again.</Text>
+                    If you do... you lose.
+                </Text>
+            </View>
         </View>
-    )
-}
-export default Index
+    );
+};
+
+export default Index;
